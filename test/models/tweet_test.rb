f73aa4125c8b218a0da4ff9325b26072db1cbe3f
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-01-28 20:38:19 -0300

    initial tests

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should have hastag' do
+    hashtag = Hashtag.new
+    assert !hashtag.save, 'saved without setting the hashtag'
+  end
 end
