7bfb32a5f20e37732180d01875214cb8da38b89e
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-01-28 16:14:50 -0300

    change to Application-only Authentication

--- app/jobs/tweet_search_job.rb
+++ app/jobs/tweet_search_job.rb
@@ -8,13 +8,13 @@ class TweetSearchJob
     @hashtags = Hashtag.all
 
     @hashtags.each do |tag|
-      search_hashtag(tag.hashtag)
+      search_hashtag(tag.hashtag, 10)
     end
   end
 
-  def self.search_hashtag(hashtag)
+  def self.search_hashtag(hashtag, number)
     puts "Hunting hashtag #{hashtag}"
-    CLIENT.search(hashtag).take(10).each do |t|
+    CLIENT.search(hashtag).take(number).each do |t|
       Tweet.create(name: t.user.screen_name,
                    tweet_id: t.id.to_s, hashtag: hashtag, content: t.text,
                    date: t.created_at)
