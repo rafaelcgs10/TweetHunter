c27f3c43fc2a49aca3bed419114006d2f10bf556
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-01-27 21:59:47 -0300

    delete hashtags feature

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -9,6 +9,14 @@ class HashtagsController < ApplicationController
     puts @tweets.inspect
   end
 
+  def destroy
+    @hashtag = Hashtag.find(params[:id])
+    @tweets = Tweet.where(hashtag: @hashtag.hashtag)
+    @tweets.destroy_all
+    @hashtag.destroy
+    redirect_to hashtags_path
+  end
+
   def create
     @hashtag = Hashtag.new(hastag_params)
     if @hashtag.save
