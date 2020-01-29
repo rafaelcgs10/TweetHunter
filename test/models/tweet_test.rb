40a0c035be2acf46c6ce7d0ed9fb97b7ddce5cbf
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-01-28 21:17:20 -0300

    unit tests tweet model

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -3,8 +3,12 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  test 'should have hastag' do
-    hashtag = Hashtag.new
-    assert !hashtag.save, 'saved without setting the hashtag'
+  test 'should not save the same tweet twice' do
+    tweet1 = Tweet.new
+    tweet1.tweet_id = '123456'
+    tweet1.save
+    tweet2 = Tweet.new
+    tweet2.tweet_id = '123456'
+    assert !tweet2.save, 'saved the same tweet a second time'
   end
 end
