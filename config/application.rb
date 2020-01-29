1a10ff98e50264b551f7c2e6f0028d932eab0c7b
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-01-29 13:40:00 -0300

    switch from minitest to rspec

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module TweetHunter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    # Change the default test framework
+    config.generators do |g|
+      g.test_framework :rspec
+    end
     # config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
