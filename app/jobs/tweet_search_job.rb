7d073a925c592efb05a0f471c6d95036ab597aca
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-01-29 12:18:42 -0300

    fix: get the full tweet content

--- app/jobs/tweet_search_job.rb
+++ app/jobs/tweet_search_job.rb
@@ -15,10 +15,16 @@ class TweetSearchJob
   def self.search_hashtag(hashtag, number)
     puts "Hunting hashtag #{hashtag}"
     hashtag.gsub(' AND ', ' ')
-    CLIENT.search(hashtag).take(number).each do |t|
-      Tweet.create(name: t.user.screen_name,
-                   tweet_id: t.id, hashtag: hashtag, content: t.text,
-                   date: t.created_at)
+    CLIENT.search(hashtag, tweet_mode: 'extended').take(number).each do |status|
+      content = if status.retweet?
+                  status.retweeted_status.attrs[:full_text]
+                elsif status.truncated? && status.attrs[:extended_tweet]
+                  status.attrs[:extended_tweet][:full_text]
+                else
+                  status.attrs[:text] || status.attrs[:full_text]
+                end
+      Tweet.create(name: status.user.screen_name, tweet_id: status.id,
+                   hashtag: hashtag, content: content, date: status.created_at)
     end
   end
 end
