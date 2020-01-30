8aa75b551be35f2999ae6c87051fabe80d6b36cc
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-01-29 23:54:39 -0300

    some features tests about hashtags

--- spec/features/hashtags_spec.rb
+++ spec/features/hashtags_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Hashtags", type: :feature do
   context 'create new hashtag' do
-    scenario 'should be successful' do
+    scenario 'should create' do
       visit new_hashtag_path
       within 'form' do
         fill_in 'Hashtag', with: '#udesc'
@@ -10,13 +10,46 @@ RSpec.feature "Hashtags", type: :feature do
       click_button 'Save Hashtag'
       expect(page).to have_content('Hashtag: #udesc')
     end
-    scenario 'should fail because input is not a hashtag' do
+
+    scenario 'should fail to create' do
       visit new_hashtag_path
       within 'form' do
         fill_in 'Hashtag', with: 'udesc'
       end
       click_button 'Save Hashtag'
-      expect(page).to have_content('Hashtag: #udesc')
+      expect(page).to have_content('field is not a valid query')
+    end
+  end
+
+  context 'checking database loaded' do
+    fixtures :hashtags
+
+    scenario 'should not have hashtag #bear' do
+      visit root_path
+      expect(page).not_to have_content('#bear')
+    end
+
+    scenario 'should have hashtag #cat' do
+      visit root_path
+      expect(page).to have_content('#cat')
+    end
+
+    scenario 'should have hashtag #dog' do
+      visit root_path
+      expect(page).to have_content('#cat')
+    end
+  end
+
+  context 'delete hashtag' do
+    fixtures :hashtags
+
+    scenario 'should delete' do
+      visit root_path
+      expect(page).to have_content('#cat')
+      within("//td[@id='delete_#cat']") do
+        click_link 'Delete'
+      end
+      expect(page).not_to have_content('#cat')
     end
   end
 end
