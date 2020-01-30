cb1c006a762fbb147b86e8b11faa31e3b89cda35
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-01-30 11:51:22 -0300

    feature tests about tweets

--- spec/features/hashtags_spec.rb
+++ spec/features/hashtags_spec.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "Hashtags", type: :feature do
+RSpec.feature 'Hashtags', type: :feature do
   context 'create new hashtag' do
     scenario 'should create' do
-      visit new_hashtag_path
+      visit root_path
+      click_link 'New hashtag'
       within 'form' do
         fill_in 'Hashtag', with: '#udesc'
       end
@@ -12,7 +15,8 @@ RSpec.feature "Hashtags", type: :feature do
     end
 
     scenario 'should fail to create' do
-      visit new_hashtag_path
+      visit root_path
+      click_link 'New hashtag'
       within 'form' do
         fill_in 'Hashtag', with: 'udesc'
       end
@@ -21,7 +25,7 @@ RSpec.feature "Hashtags", type: :feature do
     end
   end
 
-  context 'checking database loaded' do
+  context 'checking database hashtags loaded' do
     fixtures :hashtags
 
     scenario 'should not have hashtag #bear' do
@@ -40,6 +44,41 @@ RSpec.feature "Hashtags", type: :feature do
     end
   end
 
+  context 'checking database tweets loaded' do
+    fixtures :hashtags, :tweets
+
+    scenario 'should not have this tweet about #cat' do
+      visit root_path
+      within("//td[@id='show_#cat']") do
+        click_link 'Show'
+      end
+      expect(page).to_not have_content("I think your cat likes me #cat")
+    end
+
+    scenario 'should have tweet about #cat' do
+      visit root_path
+      within("//td[@id='show_#cat']") do
+        click_link 'Show'
+      end
+      within("//tr[@id='tweet_id_3']") do
+        expect(page).to have_content("I think your cat doesn't like me #cat")
+      end
+    end
+
+    scenario 'should have two tweets about #dog' do
+      visit root_path
+      within("//td[@id='show_#dog']") do
+        click_link 'Show'
+      end
+      within("//tr[@id='tweet_id_1']") do
+        expect(page).to have_content('I like my #dog')
+      end
+      within("//tr[@id='tweet_id_2']") do
+        expect(page).to have_content('I like your #dog')
+      end
+    end
+  end
+
   context 'delete hashtag' do
     fixtures :hashtags
 
@@ -49,7 +88,7 @@ RSpec.feature "Hashtags", type: :feature do
       within("//td[@id='delete_#cat']") do
         click_link 'Delete'
       end
-      expect(page).not_to have_content('#cat')
+      expect(page).to_not have_content('#cat')
     end
   end
 end
