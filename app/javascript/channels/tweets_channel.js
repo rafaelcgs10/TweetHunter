74bf2f7312897b4a74cbd6bbe898433e8becb6ed
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-02-04 14:53:04 -0300

    remove console.logs and set redistogo in resque

--- app/javascript/channels/tweets_channel.js
+++ app/javascript/channels/tweets_channel.js
@@ -3,21 +3,17 @@ import consumer from "./consumer"
 $(document).on('turbolinks:load', function () {
     if(this.subscription) {
 	consumer.subscriptions.remove(this.subscription);
-	console.log("unsubing");
     }
 
     if($('#hashtag').attr('data-hashtag-id')) {
 	const subscription = consumer.subscriptions.create({channel: "TweetsChannel", id:$('#hashtag').attr('data-hashtag-id')}, {
 	    connected() {
-		console.log("we are live on: " + $('#hashtag').attr('data-hashtag-id'));
 	    },
 
 	    disconnected() {
-		console.log("disconecting")
 	    },
 
 	    received(data) {
-		$('#tweets-card').prepend( data.content)
 	    }
 	});
 	this.subscription = subscription;
