5538b95dd72c8779fe20d0f3f05979f6d5ed803c
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-02-04 21:39:25 -0300

    tests for Query Utils

---
+++ spec/utils/query_match_util_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe QueryMatchUtil do
+  context 'valid? function' do
+    it('should fail because is not hashtag') do
+      result = QueryMatchUtil.valid? "nop"
+      expect(result).to eq(false)
+    end
+
+    it('should fail because is not empty') do
+      result = QueryMatchUtil.valid? ""
+      expect(result).to eq(false)
+    end
+
+    it('should fail because is not nil') do
+      result = QueryMatchUtil.valid? nil
+      expect(result).to eq(false)
+    end
+
+    it('should be successful because is a hashtag') do
+      result = QueryMatchUtil.valid? "#car AND (#boat OR #airplane)"
+      expect(result).to eq(true)
+    end
+  end
+
+  context 'match? function' do
+    it('should be successful because #car matches') do
+      result = QueryMatchUtil.match?("#car", "#car is fast")
+      expect(result).to eq(true)
+    end
+
+    it('should be successful because #cat AND #dog matches') do
+      result = QueryMatchUtil.match?("#cat AND #dog", "#dog is better than #cat")
+      expect(result).to eq(true)
+    end
+
+    it("should fail because #cat AND #dog doesn't matches") do
+      result = QueryMatchUtil.match?("#cat AND #dog", "#dog is better than #frog")
+      expect(result).to eq(false)
+    end
+
+    it('should be successful because #cat OR (#dog AND #frog) matches') do
+      result = QueryMatchUtil.match?("#cat OR (#dog AND #frog)", "#dog is better than #frog")
+      expect(result).to eq(true)
+    end
+  end
+
+  context 'stream_queries function' do
+    fixtures :hashtags
+    it('should be successful') do
+      result = QueryMatchUtil.stream_queries
+      expect(result).to eq("#dog,#cat")
+    end
+  end
+end
