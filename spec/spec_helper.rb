701d50770e073972b3b875615e0b22ddb3871219
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-02-05 11:45:05 -0300

    add vcr fix to codacy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,8 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'vcr_cassettes'
   config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = false
+  config.ignore_hosts 'api.codacy.com'
 end
 
 Codacy::Reporter.start
