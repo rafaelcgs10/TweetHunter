dfdfba0527e152077db012b378809a75ef4e1fa9
rafaelcgs10 <rafaelcgs10@gmail.com>
2020-02-07 14:14:31 -0300

    fix issue with subscrition in other pages else the first

--- app/javascript/channels/tweets_channel.js
+++ app/javascript/channels/tweets_channel.js
@@ -1,11 +1,13 @@
 import consumer from "./consumer"
 
+
 $(document).on('turbolinks:load', function () {
+    const params = new window.URLSearchParams(window.location.search);
     if(this.subscription) {
 	consumer.subscriptions.remove(this.subscription);
     }
 
-    if($('#hashtag').attr('data-hashtag-id')) {
+    if($('#hashtag').attr('data-hashtag-id') && (params.get('page') == 1 || params.get('page') == null)) {
 	const subscription = consumer.subscriptions.create({channel: "TweetsChannel", id:$('#hashtag').attr('data-hashtag-id')}, {
 	    connected() {
 	    },
@@ -20,4 +22,3 @@ $(document).on('turbolinks:load', function () {
 	this.subscription = subscription;
     }
 })
-
